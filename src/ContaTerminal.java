b3da9f765aee49035ab2a8278a52ce13262a5622
Rafael Arantes <rafael-fullstack@outlook.com>
2024-08-05 23:45:36 -0300

    Finalização do desafio

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,25 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Importar classe Scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da conta:");
+        int conta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o nome do cliente:");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o saldo:");
+        double saldo = scanner.nextDouble();
+  
+        //Exibir a mensagem conta criada
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + "e seu saldo "+ saldo + " já está disponível para saque.");
     }
 }
